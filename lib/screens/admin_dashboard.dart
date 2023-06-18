09f542180f0058656fbbf9fd73617a189f359ebb
ShaikhMariyam <1stimefreelancer@gmail.com>
2023-06-18 19:47:08 +0800

    sprint 3 CR

--- lib/screens/admin_dashboard.dart
+++ lib/screens/admin_dashboard.dart
@@ -2,25 +2,26 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'addHCO.dart';
 import 'addRestaurant.dart';
+import 'addProduct.dart';
 import 'HCO.dart';
 import 'restaurant.dart';
+import 'product.dart';
 
 class adminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor:
-            const Color(0xffFFD700), // Change the primary color to green
+        primaryColor: const Color(0xffFFD700),
       ),
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
         '/companies': (context) => CompaniesPage(),
-        '/products': (context) => ProductsPage(),
+        '/products': (context) => ProductPage(),
         '/restaurant': (context) => RestaurantPage(),
         '/addCompany': (context) => AddCompanyPage(),
-        '/addProduct': (context) => AddProductPage(),
+        '/addProduct': (context) => addProductPage(),
         '/addRestaurant': (context) => addRestaurantPage(),
       },
     );
@@ -36,7 +37,7 @@ class HomePage extends StatelessWidget {
         title: const Text(
           'HalalXperience',
         ),
-        centerTitle: true, // Align the text in the middle
+        centerTitle: true,
       ),
       body: Container(
         margin: const EdgeInsets.only(top: 18.0),
@@ -51,7 +52,7 @@ class HomePage extends StatelessWidget {
                 buildButton(context, 'Companies', Icons.business, '/companies'),
                 buildButton(
                     context, 'Products', Icons.shopping_basket, '/products'),
-                buildButton(context, 'Brands', Icons.label, '/restaurant'),
+                buildButton(context, 'Restaurants', Icons.label, '/restaurant'),
                 buildButton(
                     context, 'Add Company', Icons.add_business, '/addCompany'),
                 buildButton(context, 'Add Product', Icons.add_shopping_cart,
@@ -75,15 +76,14 @@ class HomePage extends StatelessWidget {
   Widget buildButton(
       BuildContext context, String label, IconData icon, String route) {
     return Container(
-      margin: const EdgeInsets.all(8.0), // Adjust the margin as needed
-      width: 70.0, // Adjust the width as needed
-      height: 70.0, // Adjust the height as needed
+      margin: const EdgeInsets.all(8.0),
+      width: 70.0,
+      height: 70.0,
       child: ElevatedButton.icon(
         style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.zero, // Remove the padding
-          shape:
-              const CircleBorder(), // Use CircleBorder to make the button circular
-          primary: Colors.yellow.shade700, // Set the background color to gold
+          padding: EdgeInsets.zero,
+          shape: const CircleBorder(),
+          primary: Colors.yellow.shade700,
         ),
         onPressed: () {
           if (route == '/addCompany') {
@@ -106,6 +106,16 @@ class HomePage extends StatelessWidget {
               context,
               MaterialPageRoute(builder: (context) => RestaurantPage()),
             );
+          } else if (route == '/addProduct') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => addProductPage()),
+            );
+          } else if (route == '/product') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RestaurantPage()),
+            );
           } else {
             Navigator.pushNamed(context, route);
           }
@@ -113,51 +123,8 @@ class HomePage extends StatelessWidget {
         icon: Icon(icon),
         label: Text(
           label,
-          style:
-              const TextStyle(fontSize: 12.0), // Adjust the font size as needed
+          style: const TextStyle(fontSize: 12.0),
         ),
-      ),
-    );
-  }
-}
-
-class ProductsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Products'),
-      ),
-      body: Center(
-        child: Text('Products Page'),
-      ),
-    );
-  }
-}
-
-class BrandsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Brands'),
-      ),
-      body: Center(
-        child: Text('Brands Page'),
-      ),
-    );
-  }
-}
-
-class AddProductPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Product'),
-      ),
-      body: Center(
-        child: Text('Add Product Page'),
       ),
     );
   }
