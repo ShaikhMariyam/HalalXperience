11ce488d5a4bd5375cd8496a851d9edca217403c
Ahmed Tarek <a7medtarek201577@gmail.com>
2023-06-28 15:51:07 +0800

    ok

--- lib/screens/user-view/products.dart
+++ lib/screens/user-view/products.dart
@@ -323,6 +323,7 @@ class ProductSearchDelegate extends SearchDelegate {
       stream: FirebaseFirestore.instance
           .collection('products')
           .where('name', isGreaterThanOrEqualTo: query)
+          .where('name', isLessThan: query + 'z')
           .snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasError) {
