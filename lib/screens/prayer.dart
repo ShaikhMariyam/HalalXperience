3add45039c1875a2b45f769b9809680455a4613b
ShaikhMariyam <1stimefreelancer@gmail.com>
2023-07-08 08:50:08 +0800

    fix teeny tiny bugs

--- lib/screens/prayer.dart
+++ lib/screens/prayer.dart
@@ -9,7 +9,7 @@ class PrayerTimesApp extends StatefulWidget {
 
 class _PrayerTimesAppState extends State<PrayerTimesApp> {
   String prayerTimesUrl =
-      'http://api.aladhan.com/v1/timingsByCity?city=Dubai&country=United Arab Emirates&method=8';
+      'http://api.aladhan.com/v1/timingsByCity/07-07-2023?city=Kuala+Lumpur&country=Malaysia&method=8';
   Map<String, String> prayerTimes = {};
   String hijriDate = '';
 
@@ -63,79 +63,93 @@ class _PrayerTimesAppState extends State<PrayerTimesApp> {
     return MaterialApp(
       title: 'Prayer Times',
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/image.jpg'),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.5),
-                BlendMode.srcOver,
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/image.jpg'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5),
+                    BlendMode.srcOver,
+                  ),
+                ),
               ),
-            ),
-          ),
-          child: Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.center,
-              child: FractionallySizedBox(
-                heightFactor: 0.45, // Adjust the height factor as needed
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Prayer Times - Dubai',
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    if (prayerTimes.isEmpty)
-                      CircularProgressIndicator()
-                    else
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          PrayerTimeItem(
-                            title: 'Fajr',
-                            time: prayerTimes['Fajr']!,
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.center,
+                child: FractionallySizedBox(
+                  heightFactor: 0.45, // Adjust the height factor as needed
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: Colors.black,
                           ),
-                          PrayerTimeItem(
-                            title: 'Sunrise',
-                            time: prayerTimes['Sunrise']!,
-                          ),
-                          PrayerTimeItem(
-                              title: 'Dhuhr', time: prayerTimes['Dhuhr']!),
-                          PrayerTimeItem(
-                            title: 'Asr',
-                            time: prayerTimes['Asr']!,
-                          ),
-                          PrayerTimeItem(
-                            title: 'Maghrib',
-                            time: prayerTimes['Maghrib']!,
-                          ),
-                          PrayerTimeItem(
-                            title: 'Isha',
-                            time: prayerTimes['Isha']!,
-                          ),
-                        ],
+                        ),
                       ),
-                    SizedBox(height: 16),
-                    Text(
-                      'Hijri Date: $hijriDate',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    Text(
-                      'Note: Prayer times are based on the Gulf Region method.',
-                      style: TextStyle(fontSize: 12, color: Colors.white),
-                    ),
-                  ],
+                      Text(
+                        'Prayer Times - Kuala Lumpur',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      SizedBox(height: 16),
+                      if (prayerTimes.isEmpty)
+                        CircularProgressIndicator()
+                      else
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            PrayerTimeItem(
+                              title: 'Fajr',
+                              time: prayerTimes['Fajr']!,
+                            ),
+                            PrayerTimeItem(
+                              title: 'Sunrise',
+                              time: prayerTimes['Sunrise']!,
+                            ),
+                            PrayerTimeItem(
+                                title: 'Dhuhr', time: prayerTimes['Dhuhr']!),
+                            PrayerTimeItem(
+                              title: 'Asr',
+                              time: prayerTimes['Asr']!,
+                            ),
+                            PrayerTimeItem(
+                              title: 'Maghrib',
+                              time: prayerTimes['Maghrib']!,
+                            ),
+                            PrayerTimeItem(
+                              title: 'Isha',
+                              time: prayerTimes['Isha']!,
+                            ),
+                          ],
+                        ),
+                      SizedBox(height: 16),
+                      Text(
+                        'Hijri Date: $hijriDate',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        'Note: Prayer times are based on the Gulf Region method.',
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
